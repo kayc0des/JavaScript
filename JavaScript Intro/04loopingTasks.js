9e2b406c74fb15e34cf4e7e58b1521115744485f
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-03-09 10:15:39 +0200

    for&while loop

---
+++ JavaScript Intro/04loopingTasks.js
@@ -0,0 +1,42 @@
+/* Task 1 */
+
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log("Counting Completed!");
+
+/* Task 2 */
+
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+}
+console.log("Countdown finished!");
+
+/* Task 3 */
+
+var counter = 1;
+
+while (counter <= 5) {
+    console.log(counter);
+    counter += 1;
+}
+console.log("Counting Completed!");
+
+/* Task 4 */
+
+var counter = 5;
+
+while (counter > 0) {
+    console.log(counter);
+    counter -= 1;
+}
+console.log("Countdown finished!");
+
+/* Task 5 */
+
+var counter = 2018;
+
+while (counter <= 2022) {
+    console.log(counter);
+    counter += 1;
+}
