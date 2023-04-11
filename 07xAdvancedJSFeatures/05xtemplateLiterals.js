23a17876398e40415139aed8c7248dd1957dd4a6
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-04-11 18:50:24 +0200

    template literals

---
+++ 07xAdvancedJSFeatures/05xtemplateLiterals.js
@@ -0,0 +1,13 @@
+// template literals
+
+let greetings = "Hello"
+let person = "Laurel"
+console.log(`${greetings} ${person}!`);
+
+//multiline strings
+let introText = `Hi, ${person},
+This is an intro to template literals in JavaScript.
+Hope you enjoy the course!
+`
+
+console.log(introText);
