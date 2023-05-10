8fb6635301343c6656c6567d34cc91085181d3b5
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-05-10 16:46:41 +0200

    updated

--- 10xTesting/Jest_Testing/timesTwo.js
+++
@@ -1,7 +0,0 @@
-/* Function to multiply number by 2 */
-
-function timesTwo(val) {
-    return val * 2;
-}
-
-module.exports = timesTwo;
--- 10xTesting/Jest_Testing/timesTwo.test.js
+++
@@ -1,7 +0,0 @@
-/* Unit Testing */
-
-const timesTwo = require('./timesTwo');
-
-test('Should return value multiplied by 2', () => {
-    expect(timesTwo(10)).toBe(20);
-})
